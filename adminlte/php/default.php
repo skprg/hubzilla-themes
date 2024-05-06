47d429a972b8f5d0ecd5eed01e8360b0aa66707b
SK <sk@utsukta.org>
2024-05-06 17:02:57 +0530

    adminlte draft commit

--- adminlte/php/default.php
+++ adminlte/php/default.php
@@ -6,7 +6,7 @@
   <title><?php if(x($page,'title')) echo $page['title'] ?></title>
   <?php if(x($page,'htmlhead')) echo $page['htmlhead'] ?>
 </head>
-<body class="dark-mode sidebar-mini sidebar-mini-md layout-navbar-fixed layout-fixed">
+<body class="dark-mode sidebar-mini sidebar-mini-md layout-navbar-fixed layout-fixed control-sidebar-push">
 <div class="wrapper">
 
   <!-- Preloader -->
